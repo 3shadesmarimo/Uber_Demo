cc4efc7805bbbd7e0a629a0a8ab8b4c74f9e25a4
Bilegt Davaa <96280882+3shadesmarimo@users.noreply.github.com>
2023-02-17 03:45:44 -0500

    Initial Commit

---
+++ Uber/App/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Bilegt Davaa on 2022-12-21.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Uber/Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Uber
+//
+//  Created by Bilegt Davaa on 2022-12-21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        UberMapViewRepresentable()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
